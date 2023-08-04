01e81d806fd840e4b718ddd88e15242ab0af635c
Tahereh Mohammadi <tahereh.mohammadi86@gmail.com>
2023-08-04 15:51:29 -0500

    Updating dipole class

--- dipole-test.py
+++ dipole-test.py
@@ -21,3 +21,39 @@ y=0
 z=0
 
 print(d.bx(x,y,z),d.by(x,y,z),d.bz(x,y,z),d.b(x,y,z))
+
+d.random(1.2)
+
+xd=[]
+yd=[]
+zd=[]
+mx=[]
+my=[]
+mz=[]
+for i in range(10000):
+    d.random(1.2)
+    xd.append(d.xd)
+    yd.append(d.yd)
+    zd.append(d.zd)
+    mx.append(d.mx)
+    my.append(d.my)
+    mz.append(d.mz)
+    print(d.xd,d.yd,d.zd,d.bx(x,y,z),d.by(x,y,z),d.bz(x,y,z))
+
+import matplotlib.pyplot as plt
+
+fig=plt.figure()
+ax=fig.add_subplot(1,2,1,projection='3d')
+ax2=fig.add_subplot(1,2,2,projection='3d')
+ax.scatter(xd,yd,zd,marker='.')
+ax2.scatter(mx,my,mz,marker='.')
+plt.show()
+
+d.set(1.2,0,0,mx[-1],my[-1],mz[-1])
+print('Final d')
+print(d.xd,d.yd,d.zd,d.bx(x,y,z),d.by(x,y,z),d.bz(x,y,z))
+    
+d.set(-1.2,0,0,mx[-1],my[-1],mz[-1])
+print('Final d')
+print(d.xd,d.yd,d.zd,d.bx(x,y,z),d.by(x,y,z),d.bz(x,y,z))
+    
--- dipole.py
+++ dipole.py
@@ -1,8 +1,17 @@
-from scipy.constants import mu_0, pi
-from numpy import sqrt
+from scipy.constants import mu_0,pi
+import numpy as np
+from random import randint,random
+from math import *
 
 class dipole:
     def __init__(self,xd,yd,zd,mx,my,mz):
+        self.xd=xd
+        self.yd=yd
+        self.zd=zd
+        self.mx=mx
+        self.my=my
+        self.mz=mz
+    def set(self,xd,yd,zd,mx,my,mz):
         self.xd=xd
         self.yd=yd
         self.zd=zd
@@ -13,7 +22,7 @@ class dipole:
         xprime=x-self.xd
         yprime=y-self.yd
         zprime=z-self.zd
-        rprime=sqrt(xprime**2+yprime**2+zprime**2)
+        rprime=np.sqrt(xprime**2+yprime**2+zprime**2)
         mdotrprime=self.mx*xprime+self.my*yprime+self.mz*zprime
         bx=mu_0/(4*pi)*(3*xprime*mdotrprime/rprime**5-self.mx/rprime**3)
         by=mu_0/(4*pi)*(3*yprime*mdotrprime/rprime**5-self.my/rprime**3)
@@ -23,7 +32,9 @@ class dipole:
         xprime=x-self.xd
         yprime=y-self.yd
         zprime=z-self.zd
-        rprime=sqrt(xprime**2+yprime**2+zprime**2)
+        #print(xprime)
+        #print(np.sqrt(xprime**2))
+        rprime=np.sqrt(xprime**2+yprime**2+zprime**2)
         mdotrprime=self.mx*xprime+self.my*yprime+self.mz*zprime
         bx=mu_0/(4*pi)*(3*xprime*mdotrprime/rprime**5-self.mx/rprime**3)
         return bx
@@ -31,7 +42,7 @@ class dipole:
         xprime=x-self.xd
         yprime=y-self.yd
         zprime=z-self.zd
-        rprime=sqrt(xprime**2+yprime**2+zprime**2)
+        rprime=np.sqrt(xprime**2+yprime**2+zprime**2)
         mdotrprime=self.mx*xprime+self.my*yprime+self.mz*zprime
         by=mu_0/(4*pi)*(3*yprime*mdotrprime/rprime**5-self.my/rprime**3)
         return by
@@ -39,7 +50,44 @@ class dipole:
         xprime=x-self.xd
         yprime=y-self.yd
         zprime=z-self.zd
-        rprime=sqrt(xprime**2+yprime**2+zprime**2)
+        rprime=np.sqrt(xprime**2+yprime**2+zprime**2)
         mdotrprime=self.mx*xprime+self.my*yprime+self.mz*zprime
         bz=mu_0/(4*pi)*(3*zprime*mdotrprime/rprime**5-self.mz/rprime**3)
         return bz
+    def random(self,a): # random position on wall half-length a
+        side=randint(0,5)
+        #print(side)
+        u=(random()-0.5)*2*a
+        v=(random()-0.5)*2*a
+        if(side==0): # floor
+            self.zd=-a
+            self.xd=u
+            self.yd=v
+        elif(side==1): # ceiling
+            self.zd=a
+            self.xd=u
+            self.yd=v
+        elif(side==2): # left side
+            self.zd=u
+            self.xd=v
+            self.yd=-a
+        elif(side==3): # right side
+            self.zd=u
+            self.xd=v
+            self.yd=a
+        elif(side==4): # front side
+            self.zd=u
+            self.xd=a
+            self.yd=v
+        else: # back side
+            self.zd=u
+            self.xd=-a
+            self.yd=v
+        #print(self.xd,self.yd,self.zd)
+        costheta=(random()-0.5)*2
+        theta=acos(costheta)
+        phi=random()*2*pi
+        self.mx=sin(theta)*cos(phi)
+        self.my=sin(theta)*sin(phi)
+        self.mz=cos(theta)
+        #print(self.mx,self.my,self.mz,sqrt(self.mx**2+self.my**2+self.mz**2))
